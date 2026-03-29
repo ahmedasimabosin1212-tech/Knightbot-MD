1ae6d331a068d05ee21f0f50cf4708a1eb2adc8c
ahmedasimabosin1212-tech <ahmedasimabosin1212@gmail.com>
2026-03-29 09:11:56 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'ASAKI Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "ASAKI Bot",
+  botOwner: 'المطور', // Your name
+  ownerNumber: '249125098282', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
